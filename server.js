ad7e13522cef29bd13e9e0339bd980478d190db9
saifmathur <saifmathur41@gmail.com>
2020-07-20 19:28:26 +0530

    changes to db.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const port = process.env.PORT || 5000
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=> console.log(`running on port ${port}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
