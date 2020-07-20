5638dd669011c124fc5d3c19b22eee44c6cd57ff
saifmathur <saifmathur41@gmail.com>
2020-07-21 02:17:45 +0530

    client side added

--- models/db.js
+++ models/db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 const connectDB = async ()=>{
     try{
-        await mongoose.connect('mongodb+srv://common:common123@summer-project-mern.shx19.mongodb.net/<dbname>?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb+srv://common:common123@summer-project-mern.shx19.mongodb.net/<dbname>?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log('Success')
     }catch (err) {
         console.log(err)
     }
 }
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 5000
-
-
-
+const connectDB = require('./models/db')
+connectDB() //running func from the db file to connect to atlas
 
 
 
